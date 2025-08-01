9c69eabfb23a7797d2d8bff0ae461ddeb9d06974
Adrian Pelaez <adrianpt1991@gmail.com>
2025-08-01 16:35:31 +0400

    starting mockup the about me section

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Hero } from './features/hero/hero';
 import { Header } from './layout/header/header';
+import { About } from './features/about/about';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Hero],
+  imports: [RouterOutlet, Header, Hero, About],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
